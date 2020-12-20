92fac61cef2cb7fcb8305cdd42c474abf0dbca73
CrowdOS <2929016824@qq.com>
2020-12-20 20:29:28 +0800

    在上一个版本上进行修改，改正跟新功能，只有当当前版本不是最新版本的时候才会提示进行版本更新，目前可以打包jar包进行运行，并且将jar包上传至服务器运行

--- src/main/java/com/example/controller/Version_UpdatingController.java
+++ src/main/java/com/example/controller/Version_UpdatingController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.entity.Version_Updating;
 import com.example.service.TaskService;
 import com.example.service.UserService;
 import com.example.service.User_TaskService;
@@ -31,96 +32,106 @@ import java.io.*;
 @RequestMapping("/version_updating")
 public class Version_UpdatingController {
 
-	@Autowired
-	private User_TaskService user_taskService;
-	@Autowired
-	private TaskService taskService;
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private Version_UpdatingService version_updatingService;
+    @Autowired
+    private User_TaskService user_taskService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private Version_UpdatingService version_updatingService;
 
-	private Object setuser1;
-
-
-	///2019.10.29  判断是否需要进行更新,前端输入当前的版本，与服务器的最新版本进行比较
-	// 版本号不同的话temp为1，进行更新，版本号相同的话temp为0，不需要费更新
-	@RequestMapping("checkVersion/{versionCode}")
-	//public ResponseEntity<List<Version_Updating>> checkVersion(@PathVariable  Integer versionCode) {
-    public ResponseEntity<String> checkVersion(@PathVariable  Integer versionCode) {
-		//return userService.SelInfo(userName).toString();
-		System.out.println(versionCode);
-		//System.out.println(userService.EnterUser(user));
-		///String类型的image获得数据库中最新的版本的版本名字，及apkName
-		String image = version_updatingService.getLastOne().getApkName();
-		System.out.println(image);
+    private Object setuser1;
 
 
+    ///2019.10.29  判断是否需要进行更新,前端输入当前的版本，与服务器的最新版本进行比较
+    // 版本号不同的话temp为1，进行更新，版本号相同的话temp为0，不需要费更新
+    @RequestMapping("checkVersion/{versionCode}")
+    //public ResponseEntity<List<Version_Updating>> checkVersion(@PathVariable  Integer versionCode) {
+    public ResponseEntity<String> checkVersion(@PathVariable Integer versionCode) {
+        //return userService.SelInfo(userName).toString();
+        System.out.println(versionCode);
+        //System.out.println(userService.EnterUser(user));
+        ///String类型的image获得数据库中最新的版本的版本名字，及apkName
+        Version_Updating version_updating = version_updatingService.getLastOne();
+        String image = version_updatingService.getLastOne().getApkName();
+        System.out.println("当前最新的APK名字：");
+        System.out.println(image);
+        if (versionCode == version_updating.getVersionCode()) {
+            System.out.println("你的版本就是最新版本，不需要更新！！！");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);///给前端返回找不到，代表着以为最新版本，不需要更新
+        }
+        if (versionCode > version_updating.getVersionCode()) {
+            System.out.println("你的版本是错的版本号，请重新发送！！！");
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);///发送的版本号大于服务器中存储的版本号，错误
+        }
 
-			if (version_updatingService.checkForPresence(versionCode) != null) {    ///不是最新版本，需要进行更新
-				System.out.println(versionCode);
-				//return new ResponseEntity<List<Version_Updating>>(
-				//version_updatingService.checkForPresence(versionCode),HttpStatus.OK); ///给前端返回OK，代表着前端可以进行更新才做，
-				// 也就是引用函数downVersionFromServer
-				String tempName = version_updatingService.getLastOne().getApkName();
-				return new ResponseEntity<>(tempName, HttpStatus.OK);
-			} else {  ///是最新版本，不需要更新
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);///给前端返回找不到，代表着以为最新版本，不需要更新
-			}
-
-		}
 
 
-	///######~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^#######
-	///2019.11.1  历史性的开创，第一次返回string类型的数据，以后也可以类推到int、float等等单个类型
-	///如果需要更新的话，返回最新的版本的versionName
-	@RequestMapping("turnName")
-	public ResponseEntity<String> turnName (){
-		String tempName = version_updatingService.getLastOne().getApkName();
-		System.out.println(tempName);
-		return new ResponseEntity<> (tempName,HttpStatus.OK);
-	}
+        if (version_updatingService.checkForPresence(versionCode) != null) {    ///不是最新版本，需要进行更新
+            System.out.println(versionCode);
+            //return new ResponseEntity<List<Version_Updating>>(
+            //version_updatingService.checkForPresence(versionCode),HttpStatus.OK); ///给前端返回OK，代表着前端可以进行更新才做，
+            // 也就是引用函数downVersionFromServer
+            String tempName = version_updatingService.getLastOne().getApkName();
+            return new ResponseEntity<>(tempName, HttpStatus.OK);
+        } else {  ///是最新版本，不需要更新
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);///给前端返回找不到，代表着以为最新版本，不需要更新
+        }
 
-	///2019.10.28  输入参数image,选择更新最新版本
-	@RequestMapping("downVersionFromServer/{image}")
-	public ResponseEntity<byte[]> downVersionFromServer(@PathVariable String image) {
-	//@RequestMapping("downVersionFromServer")
-	//public ResponseEntity<byte[]> downVersionFromServer() {
+    }
+
+
+    ///######~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^~~~~~^_^#######
+    ///2019.11.1  历史性的开创，第一次返回string类型的数据，以后也可以类推到int、float等等单个类型
+    ///如果需要更新的话，返回最新的版本的versionName
+    @RequestMapping("turnName")
+    public ResponseEntity<String> turnName() {
+        String tempName = version_updatingService.getLastOne().getApkName();
+        System.out.println(tempName);
+        return new ResponseEntity<>(tempName, HttpStatus.OK);
+    }
+
+    ///2019.10.28  输入参数image,选择更新最新版本
+    @RequestMapping("downVersionFromServer/{image}")
+    public ResponseEntity<byte[]> downVersionFromServer(@PathVariable String image) {
+        //@RequestMapping("downVersionFromServer")
+        //public ResponseEntity<byte[]> downVersionFromServer() {
 //		String suffixName = image.substring(image.lastIndexOf(File.separator));
 //		image = suffixName;
-		//2019.9.17   若是传输的参数中带有自字符  \或者/   的时候终止返回
+        //2019.9.17   若是传输的参数中带有自字符  \或者/   的时候终止返回
 
-		////2019.11.1 一次性返回数据库最后面的一条数据
-		image = version_updatingService.getLastOne().getApkName();
-		System.out.println(image);
+        ////2019.11.1 一次性返回数据库最后面的一条数据
+        image = version_updatingService.getLastOne().getApkName();
+        System.out.println(image);
 
-		boolean status1 = image.contains("/");
-		boolean status2 = image.contains("\\");
-		if( status1 || status2  ){
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-		//2019.9.15   数据流转换
-		InputStream in;
-		ResponseEntity<byte[]> response=null ;
-		String filePath = "E" + File.separator + "springboot-upload" + File.separator + "version" + File.separator ;
+        boolean status1 = image.contains("/");
+        boolean status2 = image.contains("\\");
+        if (status1 || status2) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        //2019.9.15   数据流转换
+        InputStream in;
+        ResponseEntity<byte[]> response = null;
+        String filePath = "E" + File.separator + "springboot-upload" + File.separator + "version" + File.separator;
 //		System.out.println(image);
-		File file = new File(filePath + image);
-		try {
-			in = new FileInputStream(file);
-			byte[] b=new byte[in.available()];
-			in.read(b);
-			HttpHeaders headers = new HttpHeaders();
-			String filename = new String(image.getBytes("gbk"),"iso8859-1");
-			headers.add("Content-Disposition", "attachment;filename="+filename);
-			response = new ResponseEntity<byte[]>(b, headers, HttpStatus.OK);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return response;
-	}
+        File file = new File(filePath + image);
+        try {
+            in = new FileInputStream(file);
+            byte[] b = new byte[in.available()];
+            in.read(b);
+            HttpHeaders headers = new HttpHeaders();
+            String filename = new String(image.getBytes("gbk"), "iso8859-1");
+            headers.add("Content-Disposition", "attachment;filename=" + filename);
+            response = new ResponseEntity<byte[]>(b, headers, HttpStatus.OK);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return response;
+    }
 
 }
